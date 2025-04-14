6a1781a1e0b4a3c868f7d280e99bc21fb2e7c63f
mohamedrez <mohamed.rez@gmail.com>
2025-04-14 14:58:04 +0100

    make it configurable

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :author, class_name: "Scholar", foreign_key: "author_id"
-  has_one_attached :file, service: :public_media_hetzner
+  has_one_attached :file, service: :Rails.application.config.public_storage
 
   validates :author, presence: true
 end
