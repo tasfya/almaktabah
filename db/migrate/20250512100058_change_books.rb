9d76af2d09985e8fcb9ef388d5f12a063bf8eabc
abdokin <bashbin.sh@gmail.com>
2025-05-12 16:38:47 +0100

    Fix migration

--- db/migrate/20250512100058_change_books.rb
+++ db/migrate/20250512100058_change_books.rb
@@ -1,7 +1,7 @@
 class ChangeBooks < ActiveRecord::Migration[8.0]
   def change
     change_table :books do |t|
-      t.string :title, null: false
+      t.string :title
       t.text :description
       t.string :category
       t.date :published_date
