5523202abfaa1615a6942573ba574dcaf06ba424
abdokin <bashbin.sh@gmail.com>
2025-05-13 15:53:39 +0100

    Add routes for auth

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,13 +1,14 @@
 module Api
   module V1
     class AuthenticationController < ApiController
+      skip_before_action :verify_authenticity_token
       # POST /api/v1/login
       def login
         user = User.find_by(email: login_params[:email])
 
         if user&.valid_password?(login_params[:password])
           token = JsonWebToken.encode(user_id: user.id)
-          render json: { token: token, user_id: user.id, email: user.email }, status: :ok
+          render json: { token:, user: }, status: :ok
         else
           render json: { error: "Invalid email or password" }, status: :unauthorized
         end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      skip_before_action :verify_authenticity_token, only: [:create]
+    
       # POST /api/v1/signup
       def create
         user = User.new(user_params)
@@ -13,6 +15,16 @@ module Api
           }, status: :created
         else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def me
+        if current_user
+          render json: {
+            user: ActiveModelSerializers::SerializableResource.new(current_user)
+          }, status: :ok
+        else
+          render json: { error: 'Unauthorized' }, status: :unauthorized
         end
       end
 
