b3c749ac9c94502653948e94eba1c50b7d85d212
abdokin <bashbin.sh@gmail.com>
2025-06-30 10:01:37 +0100

    Fix Avo resources input

--- app/avo/resources/lecture.rb
+++ app/avo/resources/lecture.rb
@@ -9,14 +9,13 @@ class Avo::Resources::Lecture < Avo::BaseResource
     field :id, as: :id
     field :title, as: :text
     field :description, as: :textarea
-    field :duration, as: :number
     field :category, as: :text
-    field :views, as: :number
     field :content, as: :trix
-    field :video_url, as: :text, link_to: true
-    field :video, as: :file, accept: "video/*", max_size: 100.megabytes
-    field :audio, as: :file
-    field :thumbnail, as: :file, accept: "image/*", max_size: 5.megabytes
     field :published_date, as: :date
+    field :thumbnail, as: :file, accept: "image/*", max_size: 5.megabytes
+    field :audio, as: :file, accept: "audio/*", max_size: 10.megabytes
+    field :video, as: :file, accept: "video/*", max_size: 100.megabytes
+    field :yotube_url, as: :text, help: "YouTube URL for video lessons"
+    field :video_url, as: :text, help: "URL for video lessons"
   end
 end
