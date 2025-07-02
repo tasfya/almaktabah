2c932978e861424cf6a4abb807ef6c8bac1840c8
abdokin <bashbin.sh@gmail.com>
2025-07-02 09:38:34 +0100

    Remove unused API-related specs and factories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+# Require Rails boot before environment to prevent frozen array issues
+require_relative '../config/boot'
+require 'rails/all'
+Bundler.require(*Rails.groups)
+
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
