4791989acc4480d0b3c309b0ee8b0f07173b8356
abdokin <bashbin.sh@gmail.com>
2025-07-07 16:00:18 +0100

    Add Video To benefit

--- app/models/benefit.rb
+++ app/models/benefit.rb
@@ -1,6 +1,8 @@
 class Benefit < ApplicationRecord
     has_one_attached :thumbnail, service: Rails.application.config.public_storage
     has_one_attached :audio, service: Rails.application.config.public_storage
+    has_one_attached :video, service: Rails.application.config.public_storage
+
     has_rich_text :content
 
     # Ransack configuration
