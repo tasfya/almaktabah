cf29cf061aa24b327f30f03b5ae8d54de7c00907
abdokin <bashbin.sh@gmail.com>
2025-07-10 14:12:38 +0100

    Cleanup

--- app/models/concerns/publishable.rb
+++ app/models/concerns/publishable.rb
@@ -2,13 +2,10 @@ module Publishable
   extend ActiveSupport::Concern
 
   included do
-    # Add published_at column as datetime if it doesn't exist
-    # This will be set automatically when published is changed to true
     before_save :set_published_at
 
     validates :published, inclusion: { in: [ true, false ] }
 
-    # Scopes
     scope :published, -> { where(published: true) }
     scope :unpublished, -> { where(published: false) }
   end
