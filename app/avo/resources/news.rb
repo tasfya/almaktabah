4fbdb829b21e8da02475446bc1c4c4dfcb9d896f
abdokin <bashbin.sh@gmail.com>
2025-07-10 14:22:43 +0100

    Remove duplicated field on news

--- app/avo/resources/news.rb
+++ app/avo/resources/news.rb
@@ -11,7 +11,6 @@ class Avo::Resources::News < Avo::BaseResource
     field :content, as: :trix
     field :description, as: :textarea
     field :published, as: :boolean
-    field :published_at, as: :date_time
     field :published_at, as: :date_time, help: "The date and time when this news was published", hide_on: [ :new, :edit ]
     field :thumbnail, as: :file, accept: "image/*", max_size: 5.megabytes
   end
