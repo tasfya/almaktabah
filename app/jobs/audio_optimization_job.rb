2163923c9bb5dbba04819c3f74d9d8ab919ced04
abdokin <bashbin.sh@gmail.com>
2025-07-28 11:10:05 +0100

    Make AudioOptimizationJob dont require tmp files

--- app/jobs/audio_optimization_job.rb
+++ app/jobs/audio_optimization_job.rb
@@ -1,69 +1,21 @@
-require "open-uri"
-require "fileutils"
-require "streamio-ffmpeg"
-require "securerandom"
+# frozen_string_literal: true
 
 class AudioOptimizationJob < ApplicationJob
   queue_as :default
-
-  TEMP_DIR = Rails.root.join("tmp", "audio_processing").freeze
-  AUDIO_STORAGE_DIR = Rails.root.join("tmp", "audio").freeze
 
   def perform(item)
     return unless item.audio?
     return if item.optimized_audio.attached?
 
-    FileUtils.mkdir_p(TEMP_DIR)
-    FileUtils.mkdir_p(AUDIO_STORAGE_DIR)
-
-    timestamp = Time.now.to_i
-    unique_id = SecureRandom.hex(4)
-
-    input_path = nil
-    optimized_path = nil
-
-    begin
-      item.audio.open do |audio_file|
-        input_path = write_temp_file(item, audio_file, timestamp, unique_id)
-        optimized_path = optimize_audio(input_path, timestamp, unique_id)
-
-        optimized_file_binary = File.binread(optimized_path)
-        optimized_filename = File.basename(optimized_path)
-
-        item.optimized_audio.attach(
-          io: StringIO.new(optimized_file_binary),
-          filename: optimized_filename,
-          content_type: "audio/mpeg"
-        )
-        CleanupTemporaryFilesJob.perform_later(optimized_path.to_s)
-        item.save!
-      end
-    rescue => e
-      Rails.logger.error "Audio optimization failed for item ID #{item.id}: #{e.message}"
-    ensure
-      FileUtils.rm_f(input_path) if input_path && File.exist?(input_path)
-      FileUtils.rm_f(optimized_path) if optimized_path && File.exist?(optimized_path)
+    item.audio.download do |original_io|
+      optimized_io = AudioOptimizer.new(input_io: original_io).optimize
+      original_file_name = "op_#{SecureRandom.hex(8)}.mp3"
+      item.optimized_audio.attach(
+        io:           optimized_io,
+        filename:     original_file_name,
+        content_type: "audio/mpeg"
+      )
+      item.save!
     end
-  end
-
-  private
-
-  def write_temp_file(item, audio_file, timestamp, unique_id)
-    safe_name = item.audio.filename.to_s.gsub(/[^a-zA-Z0-9\.\-_]/, "_")
-    input_filename = "#{item.class.name.underscore}_#{item.id}_#{timestamp}_#{unique_id}_#{safe_name}"
-    input_path = TEMP_DIR.join(input_filename)
-    File.write(input_path, audio_file.read, mode: "wb")
-    input_path
-  end
-
-  def optimize_audio(input_path, timestamp, unique_id)
-    base = File.basename(input_path, ".*")
-    output_filename = "op_#{base}_#{timestamp}_#{unique_id}.mp3"
-    output_path = AUDIO_STORAGE_DIR.join(output_filename)
-
-    optimizer = AudioOptimizer.new(input_path.to_s, output_path.to_s)
-    optimizer.optimize
-
-    output_path
   end
 end
