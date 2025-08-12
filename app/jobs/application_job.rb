78cd810c4d7b1c100bea5aaa699acf4b1fd546c8
mohamedrez <mohamed.rez@gmail.com>
2025-08-12 12:16:03 +0100

    fixed helpers

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,28 +6,4 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
-
-  private
-
-  def parse_integer(value)
-    Integer(value) rescue nil
-  end
-
-  def parse_datetime(value)
-    return nil unless value.present?
-    return value if value.is_a?(DateTime) || value.is_a?(Time)
-    DateTime.parse(value.to_s) rescue nil
-  end
-
-  def attach_from_url(record, attachment_name, url, content_type: nil)
-    return if url.blank?
-
-    Rails.logger.info "Enqueuing media download for #{attachment_name} from #{url} for record ##{record.id}"
-    MediaDownloadJob.perform_now(
-      record,
-      attachment_name,
-      url,
-      content_type
-    )
-  end
 end
