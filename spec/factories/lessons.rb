281b49ee07ce213f4f7578d9ea72e5f807965436
abumalick <abdullah.hilson@gmail.com>
2025-08-31 00:40:58 +0300

    fix: add video trait to lesson factory and update tests to utilize it

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -7,25 +7,28 @@ FactoryBot.define do
         duration { Faker::Number.between(from: 1, to: 100) }
         thumbnail { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'files', 'thumbnail.jpg'), 'image/jpeg') }
         audio { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'files', 'audio.mp3'), 'audio/mpeg') }
-        video { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'files', 'test_video.mp4'), 'video/mp4') }
         created_at { Time.now }
         updated_at { Time.now }
         after(:build) do |lesson|
-            lesson.series ||= create(:series) if lesson.series.nil?
+          lesson.series ||= create(:series) if lesson.series.nil?
         end
 
-        trait :without_audio do
-            audio { nil }
-        end
+    trait :with_video do
+      video { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'files', 'test_video.mp4'), 'video/mp4') }
+    end
 
-        trait :without_domain do
-            after(:create) do |lesson|
-                lesson.domains = []
-            end
-        end
+    trait :without_audio do
+      audio { nil }
+    end
 
-        after(:create) do |lesson|
-            lesson.domains = [ Domain.find_or_create_by(host: "localhost") ]
-        end
+    trait :without_domain do
+      after(:create) do |lesson|
+        lesson.domains = []
+      end
+    end
+
+    after(:create) do |lesson|
+        lesson.domains = [ Domain.find_or_create_by(host: "localhost") ]
+    end
     end
 end
