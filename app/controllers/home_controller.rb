1d76ab70a78eb46b31e7965829a89c0ec6b3fd9d
abdokin <bashbin.sh@gmail.com>
2025-09-01 17:17:48 +0100

    Refactor home controller index method for improved readability and efficiency

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @series = Series.for_domain_id(@domain.id).published.recent.first
-    @top_series = Series.for_domain_id(@domain.id).published.recent.limit(5)
+    scope = Series.for_domain_id(@domain.id).published.recent
+    @series = scope.first
+    @top_series = scope.where.not(id: @series&.id).limit(5)
     @top_lectures = Lecture.for_domain_id(@domain.id).published.recent.limit(10)
   end
 end
