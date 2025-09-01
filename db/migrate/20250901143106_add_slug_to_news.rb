31239257bbf8b5612550e6f79a9b261c7dafa295
MounirGaiby <mounirgaiby@gmail.com>
2025-09-01 15:40:14 +0100

    Add slug migration to related models

---
+++ db/migrate/20250901143106_add_slug_to_news.rb
@@ -0,0 +1,7 @@
+class AddSlugToNews < ActiveRecord::Migration[8.0]
+  def change
+    remove_column :news, :slug, :string if column_exists?(:news, :slug)
+    add_column :news, :slug, :string
+    add_index :news, :slug, unique: true
+  end
+end
