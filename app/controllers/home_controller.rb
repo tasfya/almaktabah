c48a7844b5f6a32f2f754d03fed29efc07d80ed3
Abderrahman Laraich <bashbin.sh@gmail.com>
2025-09-02 06:23:17 -0700

    Mobile improvements (#226)
    * Improve mobile design
    
    * Improve filters component for mobile devices
    
    * Implement max width for large devices across all pages
    
    * Improve news listing
    
    * Clean up header logo display for better mobile experience
    
    * Fix audio player functionality and mobile display
    
    * Fix audio player placement
    
    * Refactor social media link styles for consistency in header
    
    * Enable modern browser support in application controller
    
    * Refactor hero section layout and improve mobile responsiveness
    
    * Refactor breadcrumb helper methods for clarity and efficiency; update playlist card share button functionality
    
    * Refactor lecture and filter components for improved styling consistency and responsiveness
    
    * Refactor home controller index method for improved readability and efficiency
    
    * Fix home_controller_spec
    
    * Remove erb syntax error
    
    * Revert old header navigation
    
    ---------
    
    Co-authored-by: MounirGaiby <mounirgaiby@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @recent_lessons = Lesson.for_domain_id(@domain.id).published.limit(6).ordered_by_lesson_number
-    @recent_books = Book.for_domain_id(@domain.id).published.recent.limit(6)
-    @recent_lectures = Lecture.for_domain_id(@domain.id).published.recent.limit(6)
-    @recent_news = News.for_domain_id(@domain.id).published.recent.limit(3)
-    @recent_fatwas = Fatwa.for_domain_id(@domain.id).published.order(created_at: :desc).limit(5)
-    @featured_series = Series.for_domain_id(@domain.id).published.recent.limit(4)
-
-    @featured_lesson =  Lesson.for_domain_id(@domain.id).published.first
-    @stats = {
-      books_count: Book.for_domain_id(@domain.id).published.count,
-      lectures_count: Lecture.for_domain_id(@domain.id).published.count,
-      lessons_count: Lesson.for_domain_id(@domain.id).published.count,
-      series_count: Series.for_domain_id(@domain.id).published.count,
-      fatwas_count: Fatwa.for_domain_id(@domain.id).published.count
-    }
+    scope = Series.for_domain_id(@domain.id).published.recent
+    @series = scope.first
+    @top_series = scope.where.not(id: @series&.id).limit(5)
+    @top_lectures = Lecture.for_domain_id(@domain.id).published.recent.limit(10)
   end
 end
