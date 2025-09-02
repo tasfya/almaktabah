0b93cffac5e45aabccc88310a05abfd2f25b3735
abdokin <bashbin.sh@gmail.com>
2025-09-02 14:34:52 +0100

    Use new method for audio fallback

--- app/helpers/audio_player_helper.rb
+++ app/helpers/audio_player_helper.rb
@@ -1,6 +1,6 @@
 module AudioPlayerHelper
   def play_button(resource:, klass: "btn btn-secondary w-fit whitespace-nowrap", icon_class: "size-4")
-    return unless resource&.optimized_audio.present?
+    return unless resource&.has_any_audio?
 
     render "shared/play_button", resource: resource, klass: klass, icon_class: icon_class
   end
