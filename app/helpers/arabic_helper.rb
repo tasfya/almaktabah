f3aa6a39f60448f98eea4be0129fe0a08e9d4c16
MounirGaiby <mounirgaiby@gmail.com>
2025-09-04 18:21:38 +0100

    git commit -m "Move slug normalization to ArabicHelper"

--- app/helpers/arabic_helper.rb
+++ app/helpers/arabic_helper.rb
@@ -1,7 +1,7 @@
 module ArabicHelper
   # Arabic Unicode character ranges
-  ARABIC_LETTERS = '\u0600-\u06FF'
-  TASHKEEL_PATTERN = /[\u0610-\u061A\u064B-\u065F\u06D6-\u06DC\u06DF-\u06E8\u06EA-\u06ED]/
+  ARABIC_LETTERS = "\u0600-\u06FF".freeze
+  TASHKEEL_PATTERN = /[\u0610-\u061A\u064B-\u065F\u0670\u06D6-\u06DC\u06DF-\u06E8\u06EA-\u06ED]/.freeze
 
   ARABIC_TO_LATIN = {
     "ا" => "a", "ب" => "b", "ت" => "t", "ث" => "th",
@@ -22,5 +22,30 @@ module ArabicHelper
 
   def remove_tashkeel(text)
     text.gsub(TASHKEEL_PATTERN, "")
+  end
+
+  def normalize_for_slug(text, separator: "-")
+    str = text.to_s.strip
+
+    # Remove tashkeel (diacritics) if Arabic text is present
+    str = remove_tashkeel(str)
+
+    # Remove all punctuation (Arabic + Latin)
+    str = remove_punctuation(str)
+
+    # Keep Arabic letters, digits, and ASCII letters; replace others with separator
+    str.gsub!(/[^0-9A-Za-z#{ARABIC_LETTERS}]+/u, separator)
+
+    # Collapse repeated separators
+    str.gsub!(/#{Regexp.escape(separator)}{2,}/, separator)
+
+    # Strip leading/trailing separator
+    str.gsub!(/^#{Regexp.escape(separator)}|#{Regexp.escape(separator)}$/, "")
+
+    str.downcase
+  end
+
+  def remove_punctuation(text)
+    text.gsub(/[[:punct:]\p{P}]+/u, " ")
   end
 end
