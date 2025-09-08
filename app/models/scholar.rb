a29c4af0f13032e0572ab6528b88f32dacf90c4d
abdokin <bashbin.sh@gmail.com>
2025-09-08 10:02:04 +0100

    Fix merge problem

--- app/models/scholar.rb
+++ app/models/scholar.rb
@@ -1,9 +1,7 @@
 class Scholar < ApplicationRecord
   include Publishable
-  include DomainAssignable
 
   has_rich_text :bio
-  has_one_attached :avatar, service: Rails.application.config.public_storage
 
   # Helper method to get full name
   def name
@@ -11,10 +9,10 @@ class Scholar < ApplicationRecord
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "created_at", "first_name", "id", "last_name", "updated_at", "published", "published_at" ]
+    [ "created_at", "first_name", "id", "last_name", "updated_at" ]
   end
 
   def self.ransackable_associations(auth_object = nil)
-    [ "domain_assignments", "domains" ]
+    []
   end
 end
