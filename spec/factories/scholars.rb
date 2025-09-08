54fafbf9bd2be9dd7e247c26b1b9eeff321c3e37
abdokin <bashbin.sh@gmail.com>
2025-09-08 15:06:12 +0100

    Refactor blob extension fix task and update scholar factory to ensure published attribute is always true

--- spec/factories/scholars.rb
+++ spec/factories/scholars.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     bio { Faker::Lorem.sentence }
-    published
+    published { true }
   end
 end
