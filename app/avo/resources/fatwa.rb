a8e6aaf598d48807d79e8b31a3546e0077378fda
abdokin <bashbin.sh@gmail.com>
2025-10-02 11:59:59 +0100

    refactor: improve Fatwa resource fields and filters for better organization and search functionality

--- app/avo/resources/fatwa.rb
+++ app/avo/resources/fatwa.rb
@@ -1,23 +1,35 @@
 class Avo::Resources::Fatwa < Avo::BaseResource
-  # self.includes = []
-  # self.attachments = []
-  # self.search = {
-  #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
-  # }
+  self.title = :title
+  self.includes = [ :scholar ]
+  self.search = {
+    query: -> { query.ransack(id_eq: params[:q], title_cont: params[:q], category_cont: params[:q], m: "or").result(distinct: false) }
+  }
+  self.default_view_type = :table
+  self.visible_on_sidebar = true
+
+  def filters
+    filter Avo::Filters::AutoFilter
+    filter Avo::Filters::ScholarFilter
+    filter Avo::Filters::PublishedFilter
+    filter Avo::Filters::CategoryFilter
+    filter Avo::Filters::DateRangeFilter
+  end
 
   def fields
-    field :id, as: :id
-    field :scholar, as: :belongs_to
-    field :title, as: :text
-    field :category, as: :text
+    field :id, as: :id, sortable: true
+    field :title, as: :text, sortable: true, searchable: true
+    field :category, as: :text, sortable: true, searchable: true
     field :question, as: :trix
     field :answer, as: :trix
-    field :published, as: :boolean
-    field :published_at, as: :date_time, help: "The date and time when this fatwa was published", hide_on: [ :new, :edit ]
+    field :published, as: :boolean, sortable: true
+    field :scholar, as: :belongs_to, sortable: true, searchable: true
+    field :published_at, as: :date_time, help: "The date and time when this fatwa was published", hide_on: [ :new, :edit ], sortable: true
     field :audio, as: :file, accept: "audio/*", max_size: 10.megabytes
     field :video, as: :file, accept: "video/*", max_size: 100.megabytes
-    field :generated_video, as: :file, accept: "video/*", max_size: 100.megabytes, hide_on: [ :new, :edit ], readonly: true
+    field :source_url, as: :text, help: "Source URL for the fatwa"
     field :optimized_audio, as: :file, accept: "audio/*", max_size: 10.megabytes, hide_on: [ :new, :edit ], readonly: true
+    field :created_at, as: :date_time, hide_on: [ :new, :edit ], sortable: true
+    field :updated_at, as: :date_time, hide_on: [ :new, :edit ], sortable: true
   end
 
   def actions
