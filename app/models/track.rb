50b025a792607cf776c17b6c08cadce190a3c7ac
mohamedrez <mohamed.rez@gmail.com>
2025-12-15 21:29:30 +0100

    update brakeman

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,6 @@ class Track < ApplicationRecord
   enum :difficulty_level, { beginner: 0, intermediate: 1, advanced: 2, expert: 3 }
   has_many :track_series, dependent: :destroy
   has_many :series, through: :track_series
+
+  has_rich_text :description
 end
