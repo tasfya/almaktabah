321e01d36034ad3713d81d98a15905784241b8e2
abumalick <abdullah.hilson@gmail.com>
2025-12-18 22:02:21 +0300

    fix: schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_05_075738) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_15_173451) do
   create_table "action_logs", force: :cascade do |t|
     t.string "action"
     t.string "actionable_type", null: false
