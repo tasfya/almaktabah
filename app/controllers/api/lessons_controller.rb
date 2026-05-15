4088272baf8db6509d6f716442833fbd15b7198f
mohamedrez <mohamed.rez@gmail.com>
2026-05-15 10:48:21 +0100

    lesson controler

---
+++ app/controllers/api/lessons_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Api
+  class LessonsController < BaseController
+    def pending_downloads
+      lessons = Lesson.with_youtube_url_missing_video
+                      .select(:id, :title, :youtube_url)
+                      .limit(params[:limit] || 50)
+
+      render json: {
+        lessons: lessons.map { |l| { id: l.id, title: l.title, youtube_url: l.youtube_url } },
+        count: lessons.size
+      }
+    end
+
+    def upload_video
+      lesson = Lesson.find(params[:id])
+
+      unless params[:video].present?
+        return render json: { error: "No video file provided" }, status: :unprocessable_entity
+      end
+
+      lesson.video.purge if lesson.video.attached?
+      lesson.video.attach(params[:video])
+
+      if params[:thumbnail].present?
+        lesson.thumbnail.purge if lesson.thumbnail.attached?
+        lesson.thumbnail.attach(params[:thumbnail])
+      end
+
+      if lesson.save
+        render json: { success: true, lesson_id: lesson.id, video_attached: lesson.video.attached? }
+      else
+        render json: { error: lesson.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  end
+end
