b8c5fdc5aec07014516c0cb950deae09b9cb64af
mohamedrez <mohamed.rez@gmail.com>
2026-07-02 12:53:51 +0100

    fix csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,8 +11,9 @@ Rails.application.configure do
       policy.connect_src :self, :https, "http://localhost:3036", "ws://localhost:3036", "http://127.0.0.1:3036", "ws://127.0.0.1:3036", "http://127.0.0.1:3000"
       policy.style_src :self, :unsafe_inline, "http://localhost:3036", "http://127.0.0.1:3036", "http://127.0.0.1:3000"
     else
-      policy.script_src :self
+      policy.script_src :self, "https://www.googletagmanager.com", "https://www.google-analytics.com"
       policy.style_src :self, :unsafe_inline
+      policy.connect_src :self, :https, "https://www.google-analytics.com", "https://www.googletagmanager.com"
     end
   end
 
